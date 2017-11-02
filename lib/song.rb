f9a31295306a55418da5eec68c573fb6ef047864
github username <github email address>
2017-11-01 22:27:29 -0400

    Completed advanced class methods lab

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,66 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    return song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      return self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|word| word.name}
+  end
+
+  def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+
+    song = self.new_by_name(song_array[1].split(".")[0])
+    #binding.pry
+    song.artist_name = song_array[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_array = filename.split(" - ")
+
+    song = self.create_by_name(song_array[1].split(".")[0])
+    #binding.pry
+    song.artist_name = song_array[0]
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    @@all = []
   end
 
 end
